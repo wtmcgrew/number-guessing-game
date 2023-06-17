22cb21802028c7b285335b8d887efed1ef2add23
Whitney McGrew <wt.mcgrew@gmail.com>
2023-06-16 22:49:56 -0500

    Function logic for reset button, prevventDefault logic for submit button

--- script.js
+++ script.js
@@ -1,37 +1,47 @@
-let messageOne = document.getElementById("message-1");
+let message = document.getElementById("message");
 let messageTwo = document.getElementById("message-2");
-let messageThree = document.getElementById("message-3");
 
-let answer = Math.floor(Math.random() * 100) + 1;
+let answer = Math.floor(Math.random() * 100) + 1; 
+/*
+Math.random() Generates a number between 0 and 1 (not including 0)
+Multiples by 100 since we're looking for a number tween 1-100
+Math.floor() Rounds this answer down to the nearest whole number
+Adding 1 gets a whole number between 1 and 100
+*/
+
 let numberOfGuesses = 0;
 let guessedNumbers = [];
 
 function tries() {
-	let userGuess = document.getElementById("guess").value;
+	event.preventDefault(); // Prevents submit button from being actually submitted as a form
 
-	if (userGuess < 1 || userGuess > 100) {
-		alert("Please enter a number between 1 and 100");
+	let userGuess = document.getElementById("guess").value; // Grabs value of number that is submitted
+
+	if (userGuess ==- null) { // Test case tif submit is clicked with nothing inside box
+		alert("PLease enter a number");
 	}
 	else {
-		guessedNumbers.push(userGuess);
+		guessedNumbers.push(userGuess) // Will show input on screen for user to see what numbers have been entered
 		numberOfGuesses += 1;
-
+		// Sets number of tries to 3
 		if (numberOfGuesses > 3) {
-			alert("You are out of tries!");
+			alert("You are out of tries. Click reset and try again!");
 		}
 		else if (userGuess < answer) {
-			messageOne.textContent = "Your guess is too low";
-			messageTwo.textContent = "No. of Guesses: " + numberOfGuesses;
-			messageThree.textContent = "Guessed Numbers Are: " + guessedNumbers;
+			message.textContent = "Your guess is too low";
+			messageTwo.textContent = "Guessed Numbers Are: " + guessedNumbers;
 		}
 		else if (userGuess > answer) {
-			messageOne.textContent = "Your guess is too high";
-			messageTwo.textContent = "No. of Guesses: " + numberOfGuesses;
-			messageThree.textContent = "Guessed Numbers Are: " + guessedNumbers;
+			message.textContent = "Your guess is too high";
+			messageTwo.textContent = "Guessed Numbers Are: " + guessedNumbers;
 		}
 		else if (userGuess === answer) {
-			messageOne,textContent = "That's correct!";
-			messageTwo,textContent = "The correct answer is " + answer;
+			message.textContent = "That's correct!";
+			alert("Click reset to play again!");
 		}
 	}
+}
+
+function myButton () { // Resets form/page
+	location.reload();
 }
